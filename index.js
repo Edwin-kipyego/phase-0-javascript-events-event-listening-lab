4bd7f9ac1ba7fcfbd0125f46448a769e5626a48d
Edwin kipyego <koriredwin95@gmail.com>
2025-03-15 16:25:31 +0300

    js event lab test

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+    const element = document.getElementById('button');
+  element.addEventListener('click', function() {
+    console.log('button clicked!');
+  });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+addingEventListener();
+
